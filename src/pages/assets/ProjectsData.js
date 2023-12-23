6159d5120c2fb285f3f8a5fd9bda9e67a5a6dd79
enamia22 <aimane_essaidi@hotmail.com>
2023-12-23 16:28:57 +0100

    some changes

--- src/pages/assets/ProjectsData.js
+++ src/pages/assets/ProjectsData.js
@@ -1,45 +1,48 @@
-import consultu from '../../Image/proj_imgs/Project.png';
-import gifty from '../../Image/proj_imgs/Screenshot (316).png';
-import bo from '../../Image/proj_imgs/123.png';
-import art from '../../Image/proj_imgs/art.jpeg';
-
+import consultu from "../../Image/proj_imgs/Project.png";
+import gifty from "../../Image/proj_imgs/Screenshot (316).png";
+import bo from "../../Image/proj_imgs/123.png";
+import art from "../../Image/proj_imgs/art.jpeg";
 
 const proj_data = [
   {
     id: 1,
-    project_name: 'Consult U',
-    proj_creation_date: 'June 2023',
+    project_name: "Consult U",
+    proj_creation_date: "June 2023",
     project_image: consultu,
-    proj_description: "Ceci est une platforme pour la gestion des cours a distance.",
-    proj_code_like: '',
-    proj_live_link: '',
+    proj_description:
+      "ConsultU connecte de manière transparente les étudiants et les enseignants pour des consultations en ligne, en privilégiant l'efficacité et l'accessibilité. Il utilise une pile technologique robuste, comprenant MySQL, PHP, Laravel, PHPMyAdmin et Bootstrap. Cette fusion garantit non seulement un stockage et une récupération fiables des données, mais également une interface utilisateur organisée et visuellement attrayante. Les étudiants peuvent facilement se connecter, vérifier la disponibilité des enseignants et planifier des consultations via une plateforme intuitive. ConsultU sert de plateforme centralisée, favorisant une communication rationalisée entre les étudiants et les enseignants pour un soutien académique efficace, tout en optimisant l'expérience utilisateur dans le paysage éducatif dynamique.",
+    proj_code_like: "",
+    proj_live_link: "",
   },
   {
     id: 2,
-    project_name: 'Gifty Store',
-    proj_creation_date: 'Decembre 2023',
+    project_name: "Gifty Store",
+    proj_creation_date: "Decembre 2023",
     project_image: gifty,
-    proj_description: "Ceci est une platforme pour la gestion des cours a distance.",
-    proj_code_like: '',
-    proj_live_link: '',
+    proj_description:
+      "Notre site de vente d'accessoires en ligne, construit avec la stack MERN, propose une expérience utilisateur riche en fonctionnalités. Les clients peuvent créer une wishlist personnalisée pour sauvegarder leurs articles préférés. Un filtre de recherche avancé facilite la découverte des produits, rendant la navigation dans le catalogue plus efficace. En outre, nous avons intégré un générateur de cartes de vœux, offrant une dimension personnelle à chaque achat. Cette combinaison de fonctionnalités, associée à la robustesse de la stack MERN, garantit une plateforme d'e-commerce réactive et complète, répondant aux besoins divers de nos utilisateurs.",
+    proj_code_like: "",
+    proj_live_link: "",
   },
   {
     id: 3,
-    project_name: 'Backoffice website',
-    proj_creation_date: 'Novembre 2023',
+    project_name: "Backoffice website",
+    proj_creation_date: "Novembre 2023",
     project_image: bo,
-    proj_description: "Ceci est une platforme pour la gestion des cours a distance.",
-    proj_code_like: '',
-    proj_live_link: '',
+    proj_description:
+      "Le site back office que j'ai conçu avec la stack MERN simplifie la création et la maintenance des produits tout en offrant un accès transparent aux commandes et aux profils clients. Grâce à MongoDB, la gestion des produits est flexible et efficace, tandis qu'Express.js et Node.js assurent un back-end robuste. Du côté front-end, React offre une interface utilisateur intuitive, permettant une navigation aisée pour la création rapide de produits et la gestion des commandes en cours. Ce site back office, propulsé par la stack MERN, facilite considérablement les opérations quotidiennes en offrant une solution complète pour les professionnels souhaitant optimiser la gestion de leurs produits et de leur clientèle.",
+    proj_code_like: "",
+    proj_live_link: "",
   },
   {
     id: 4,
-    project_name: 'Multiple mini projet: galerie, clone facebook...',
-    proj_creation_date: 'May 2023',
+    project_name: "Multiple mini projet: galerie, clone facebook...",
+    proj_creation_date: "May 2023",
     project_image: art,
-    proj_description: "Ceci est une platforme pour la gestion des cours a distance.",
-    proj_code_like: '',
-    proj_live_link: '',
+    proj_description:
+      "Mon expérience inclut la participation à divers mini-projets où j'ai travaillé avec différents langages de programmation et technologies. Ces projets m'ont permis d'explorer des domaines tels que JavaScript, PHP et Python, ainsi que des frameworks populaires comme React et Laravel. En naviguant à travers cette diversité technologique, j'ai développé une adaptabilité et une expertise technique variée. Ces expériences m'ont non seulement offert une compréhension approfondie des nuances du développement, mais ont également renforcé ma passion pour l'apprentissage continu et la résolution créative de problèmes techniques.",
+    proj_code_like: "",
+    proj_live_link: "",
   },
 ];
 
